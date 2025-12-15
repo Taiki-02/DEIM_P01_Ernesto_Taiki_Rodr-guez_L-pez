858e6a4467f4e48b2b3802157f6289d5eed4bdc1
Taiki <>
2025-12-15 17:30:00 +0100

    Examen

--- Proyecto compartido Taiki y Miguel/Assets/Scripts/Player controler.cs
+++ Proyecto compartido Taiki y Miguel/Assets/Scripts/Player controler.cs
@@ -1,4 +1,10 @@
+using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Playercontroler : MonoBehaviour
@@ -6,6 +12,8 @@ public class Playercontroler : MonoBehaviour
     public float speed = 5.5f;
 
     private Rigidbody2D rb;
+
+    private bool salto;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,18 +24,21 @@ public class Playercontroler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        screenbordermovement();
+        //Fingermovement();
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             rb.linearVelocityX = -speed;
 
 
         }
-        else if (Input.GetKeyUp(KeyCode.A)) 
+        else if (Input.GetKeyUp(KeyCode.A))
         {
 
 
-            rb.linearVelocityX = 0;       
-        
+            rb.linearVelocityX = 0;
+
         }
 
         if (Input.GetKeyDown(KeyCode.D))
@@ -55,6 +66,27 @@ public class Playercontroler : MonoBehaviour
         //}
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Moneda"))
+        {
+            Destroy(collision.gameObject);
+
+            print("+10 puntos");
+
+        }
+
+        if (collision.gameObject.CompareTag("PlataformaSalto"))
+        {
+
+            salto = true;
+            
+            
+
+
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Señal"))
@@ -64,14 +96,82 @@ public class Playercontroler : MonoBehaviour
 
         }
 
-        if (collision.gameObject.CompareTag("Moneda"))
+        if (collision.gameObject.CompareTag("PlataformaSalto"))
         {
-            Destroy(gameObject);
+            
 
-            print("+10 puntos");
+
 
         }
+
+    }
+
+    private void Fingermovement()
+    {
+
+        //Comprueba si toca la pantalla
+        if (Input.touchCount > 0)
+        {
+            float fingerMovementX = Input.touches[0].deltaPosition.x;
+
+            rb.linearVelocityX = fingerMovementX * speed;
+        }
+        else
+        {
+
+
+            rb.linearVelocityX = 0;
+
+
+
+        }
+
+
+
+
+
     }
 
 
+
+
+
+    private void screenbordermovement()
+    {
+        //Comprueba si toca la pantalla
+        if (Input.touchCount > 0)
+        {
+            float touchscreenPositionX = Input.touches[0].position.x;
+            float screenCenter = Screen.width / 2;
+
+            if (touchscreenPositionX > screenCenter)
+            {
+
+                rb.linearVelocityX = speed;
+
+
+
+            }
+            else
+            {
+
+
+                rb.linearVelocityX = -speed;
+
+
+
+            }
+
+        }
+        else
+        {
+
+
+            rb.linearVelocityX = 0;
+
+
+
+        }
+
+    }
 }
